9032396b51bd831b43de7b410fbd5cc493b6821a
Antoine Chassagne <antoine.chassagne@me.com>
2019-12-05 18:46:48 +0100

    WIP API service

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { fetchRoverPhotos, fetchAllRoverPhotos } from './api.js';
 import './App.css';
 
 function App() {
+  fetchAllRoverPhotos({ earth_date: '2019-7-12' });
   return (
     <div>
       <header>
