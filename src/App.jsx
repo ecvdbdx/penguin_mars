e773215ab2e3f9b7de3fc70d6c3eba5e4d5dd1dd
Alex Dean <alex.dean9g@gmail.com>
2019-12-12 13:45:14 +0100

    created card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { fetchRoverPhotos, fetchAllRoverPhotos } from './api.js';
+import Card from './components/Card';
 import './App.css';
 
 function App() {
   fetchAllRoverPhotos({ earth_date: '2019-7-12' });
   return (
-    <div>
+    <div>x
       <header>
         <h1>Mars App</h1>
       </header>
